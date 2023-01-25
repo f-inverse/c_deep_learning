b5a9cc258a3102bbff6ac1210c59f9aacd8dc467
Ansu6000 <111262387+Ansu6000@users.noreply.github.com>
2023-01-25 20:47:38 +0530

    Add files via upload

---
+++ cml3_/cml3-main/include/multiple_nn_out.h
@@ -0,0 +1,17 @@
+#ifndef _MULTIPLE_NN_OUT_H
+#define _MULTIPLE_NN_OUT_H
+#define IN_LEN 3
+#define OUT_LEN 3
+
+double single_in_single_out(double input, double weight);
+double multiple_input_single_output(double * input, double * weight, int LEN);
+void single_in_multiple_out_nn(double * input_scaler, double * weight_vector, double * output_vector, int LEN);
+void multiple_input_multiple_output_nn(double * input_vector,
+                                       int INPUT_LEN,
+                                       double * output_vector,
+                                       int OUTPUT_LEN,
+                                       double weight_matrix[][IN_LEN]);
+
+#endif
+
+
---
+++ cml3_/cml3-main/src/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#define IN_LEN 3
+#define OUT_LEN 3
+#include "/root/cml3/include/multiple_nn_out.h"
+#include "/root/cml3/src/multiple_nn_out.cpp"
+#define Sad 0.9
+
+#define SAD_PREDICTION_IDX 0
+#define SICK_PREDICTION_IDX    1
+#define ACTIVE_QUALITY_PREDICTION_IDX 2
+
+
+double predicted_results[3];
+
+                                    //temp hum   air_q
+double weights[OUT_LEN][IN_LEN] = {{-2,   9.5,   2.01}, // sad?
+                                   {-0.8, 7.2,   6.3}, // sick?
+                                   {-0.5,  0.45,  0.9} //  active?
+                                   };
+
+double inputs[IN_LEN] = {30, 87, 110}; // (temp, hum, air_q) 
+int main()
+{
+    multiple_input_multiple_output_nn(inputs, IN_LEN, predicted_results, OUT_LEN, weights);
+    printf("Sad Prediction : %f \r\n",predicted_results[SAD_PREDICTION_IDX]);
+    printf("Sick Prediction : %f \r\n",predicted_results[SICK_PREDICTION_IDX]);
+    printf("Active Prediction : %f \r\n",predicted_results[ACTIVE_QUALITY_PREDICTION_IDX]);
+    return 0;
+}
